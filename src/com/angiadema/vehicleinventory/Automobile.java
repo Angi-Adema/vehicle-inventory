36d94c3a2c751eac8a3ec4dbd63237564cefe272
Angi-Adema <dr.adema@gmail.com>
2026-03-04 20:12:48 -0700

    added Main.java class as well as added removeVehicle method to Automobile.java

--- src/com/angiadema/vehicleinventory/Automobile.java
+++ src/com/angiadema/vehicleinventory/Automobile.java
@@ -11,13 +11,69 @@ public class Automobile {
 	
 	// Default constructor
 	public Automobile() {
-		
+		this.make = "";
+		this.model = "";
+		this.color = "";
+		this.year = 0;
+		this.mileage = 0;
 	}
 	
 	// Parameterized constructor
-	
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public Automobile(String make, String model, String color, int year, int mileage) {
+		this.make = make;
+		this.model = model;
+		this.color = color;
+		this.year = year;
+		this.mileage = mileage;
 	}
+	
+	// addNewVehicle method setting the field values
+	public String addNewVehicle(String make, String model, String color, int year, int mileage) {
+		try {
+			this.make = make;
+			this.model = model;
+			this.color = color;
+			this.year = year;
+			this.mileage = mileage;
+			return "Vehicle successfully added.";
+		} catch (Exception e) {
+			return "Error adding vehicle, please try again.";
+		}
+	}
+	
+	// List vehicle information by returning a String array
+	public String[] listVehicleInfo() {
+		try {
+			String[] info = new String[5];
+			
+			info[0] = make;
+			info[1] = model;
+			info[2] = color;
+			info[3] = Integer.toString(year);
+			info[4] = Integer.toString(mileage);
+			
+			return info;
+			
+		} catch (Exception e) {
+			return new String[] { "Obtaining vehicle details failed, please try again."};
+		}	
+	}
+	
+	// Remove a vehicle from the inventory
+	public String removeVehicle() {
+		try {
+			this.make = "";
+			this.model = "";
+			this.color = "";
+			this.year = 0;
+			this.mileage = 0;
+			return "Vehicle successfully removed.";
+		} catch (Exception e) {
+			return "Unable to remove vehicle, please try again.";
+		}
+	}
+			
+		
+	
+
 }
