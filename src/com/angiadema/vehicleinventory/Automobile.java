67eb0e2899caed6fc4eebaa3f631ca4a90d784a4
Angi-Adema <dr.adema@gmail.com>
2026-03-04 18:45:32 -0700

    created Automobile class, declared variables and default / parameterized constructors

---
+++ src/com/angiadema/vehicleinventory/Automobile.java
@@ -0,0 +1,23 @@
+package com.angiadema.vehicleinventory;
+
+public class Automobile {
+	
+	// Declare variables
+	private String make;
+	private String model;
+	private String color;
+	private int year;
+	private int mileage;
+	
+	// Default constructor
+	public Automobile() {
+		
+	}
+	
+	// Parameterized constructor
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
